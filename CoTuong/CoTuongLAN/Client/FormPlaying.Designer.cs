f260ca05821729e24d3b86a0676fe6bde77b7704
sontapcode04 <daocongson10012004@gmail.com>
2024-05-05 21:58:41 +0700

    new

---
+++ CoTuong/CoTuongLAN/Client/FormPlaying.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Client
+{
+    partial class FormPlaying
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelSide0 = new System.Windows.Forms.Label();
+            this.labelSide1 = new System.Windows.Forms.Label();
+            this.labelOrder = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(772, 797);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormPlaying_MouseDown);
+            // 
+            // labelSide0
+            // 
+            this.labelSide0.AutoSize = true;
+            this.labelSide0.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSide0.Location = new System.Drawing.Point(819, 72);
+            this.labelSide0.Name = "labelSide0";
+            this.labelSide0.Size = new System.Drawing.Size(142, 29);
+            this.labelSide0.TabIndex = 1;
+            this.labelSide0.Text = "Labelside0";
+            // 
+            // labelSide1
+            // 
+            this.labelSide1.AutoSize = true;
+            this.labelSide1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSide1.Location = new System.Drawing.Point(819, 135);
+            this.labelSide1.Name = "labelSide1";
+            this.labelSide1.Size = new System.Drawing.Size(139, 29);
+            this.labelSide1.TabIndex = 2;
+            this.labelSide1.Text = "labelSide1";
+            // 
+            // labelOrder
+            // 
+            this.labelOrder.AutoSize = true;
+            this.labelOrder.BackColor = System.Drawing.Color.Transparent;
+            this.labelOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOrder.ForeColor = System.Drawing.Color.Black;
+            this.labelOrder.Location = new System.Drawing.Point(817, 225);
+            this.labelOrder.Name = "labelOrder";
+            this.labelOrder.Size = new System.Drawing.Size(202, 42);
+            this.labelOrder.TabIndex = 9;
+            this.labelOrder.Text = "labelOrder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(817, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "labelOrder";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(779, 450);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(342, 164);
+            this.listBox1.TabIndex = 5;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(862, 697);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(115, 65);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(967, 629);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 7;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(779, 630);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // FormPlaying
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1272, 797);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelOrder);
+            this.Controls.Add(this.labelSide1);
+            this.Controls.Add(this.labelSide0);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormPlaying";
+            this.Text = "FormPlaying";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormPlaying_FormClosing);
+            this.Load += new System.EventHandler(this.FormPlaying_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormPlaying_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelSide0;
+        private System.Windows.Forms.Label labelSide1;
+        private System.Windows.Forms.Label labelOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CoTuong/CoTuongLAN/Client/Service.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public class Service
+    {
+        ListBox listbox;
+        StreamWriter sw;
+        public Service(ListBox listbox, StreamWriter sw)
+        {
+            this.listbox = listbox;
+            this.sw = sw;
+        }
+        // send data to server
+        public void SendToServer(string str)
+        {
+            try
+            {
+                sw.WriteLine(str);
+                sw.Flush();
+            }
+            catch
+            {
+                AddItemToListBox("Failed to send data");
+            }
+        }
+        delegate void ListBoxDelegate(string str);
+        //Write information to listBox
+        public void AddItemToListBox(string str)
+        {
+            if (listbox.InvokeRequired)
+            {
+                ListBoxDelegate d = AddItemToListBox;
+                listbox.Invoke(d, str);
+            }
+            else
+            {
+                listbox.Items.Add(str);
+                listbox.SelectedIndex = listbox.Items.Count - 1;
+                listbox.ClearSelected();
+            }
+        }
+    }
+}
